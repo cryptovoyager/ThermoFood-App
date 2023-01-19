10ae0e19846868fb18b55fb7ae5c51de51a11591
Raul <raulmayas20@gmail.com>
2023-01-19 12:30:02 -0600

    Creating filtering for different number of instructions and ingredients

--- assets/js/scripts/app.js
+++ assets/js/scripts/app.js
@@ -1,15 +1,16 @@
 var locBtn = document.querySelector(".location");
 var dummyText = document.getElementById("dummy");
 
+
 function getLocation() {
   if (navigator.geolocation) {
-    navigator.geolocation.getCurrentPosition(getApi, denial);
+    navigator.geolocation.getCurrentPosition(getApi, denyLocation);
   } else {
     dummyText.innerHTML = "Geolocation is not supported by this browser.";
   }
 }
 
-function denial() {
+function denyLocation() {
   alert("deny");
 }
 
@@ -27,15 +28,26 @@ function getApi(position) {
       console.log(data.city.name);
       console.log(data.list[0].main.temp);
       var temperature = parseFloat(data.list[0].main.temp);
-      var city = (data.city.name);
+      var city = data.city.name;
 
       getFood(temperature, city);
     });
 }
 
 function getFood(temperature, city) {
-  var dishesIndex = Math.floor(Math.random() * noLocation.length);
-  console.log(dishesIndex)
+  var ingredients = [];
+  var instructions = []
+  if (temperature < 10) {
+    var dishes = coldDays;
+  } else if (temperature >= 10 && temperature < 25) {
+    var dishes = coolDays;
+  } else if (temperature >= 25 && temperature < 35) {
+    var dishes = warmDays;
+  } else {
+    var dishes = hotDays;
+  }
+  var dishesIndex = Math.floor(Math.random() * dishes.length);
+  console.log(dishesIndex);
   var tag = noLocation[dishesIndex];
   console.log(tag);
 
@@ -58,28 +70,58 @@ function getFood(temperature, city) {
     .then(function (data) {
       var index = Math.floor(Math.random() * data.results.length);
       console.log(data.results[index]);
+      console.log("components", data.results[index].sections[0].components);
+      for (var i = 0; i < data.results[index].sections.length; i++) {
+        console.log("section " + data.results[index].sections[i].name);
+        for (
+          var j = 0;
+          j < data.results[index].sections[i].components.length;
+          j++
+        ) {
+          ingredients.push(
+            data.results[index].sections[i].components[j].raw_text
+          );
+        }
+      }
+      
+
+      console.log(data.results[index].instructions);
+      for (var i = 0; i < data.results[index].instructions.length; i++) {
+        console.log(
+          "Instructions " + (i + 1) + ": " + data.results[index].instructions[i].display_text
+        );
+        instructions.push(
+          data.results[index].instructions[i].display_text
+        );
+      }
+      console.log("Ingredients List:", ingredients);
+      console.log("Instructions:", instructions);
       dummyText.innerHTML = `<b>City: </b> ${city}<br>
       <b>Temperature: </b> ${temperature} C°<br>
       <b>Suggested Dish: </b> ${data.results[index].name}<br>
       <b>Dish Description: </b> ${data.results[index].description}<br>
       <img src=${data.results[index].thumbnail_url} width=200px height=200px><br>
       <h2><u>Ingredients</u></h2>
-      <ul>
-      <li>${data.results[index].sections[0].components[0].raw_text}</li>
-      <li>${data.results[index].sections[0].components[1].raw_text}</li>
-      <li>${data.results[index].sections[0].components[2].raw_text}</li>
-      <li>${data.results[index].sections[0].components[3].raw_text}</li>
-      <li>${data.results[index].sections[0].components[4].raw_text}</li>
+      <ul id="ingredientsList">
       </ul>
       <h2><u>Instructions</u></h2>
-      <ul>
-      <li>${data.results[index].instructions[0].display_text}</li>
-      <li>${data.results[index].instructions[1].display_text}</li>
-      <li>${data.results[index].instructions[2].display_text}</li>
-      <li>${data.results[index].instructions[3].display_text}</li>
-      <li>${data.results[index].instructions[4].display_text}</li>
-      </ul>`;;
-      
+      <ol id="instructionList">
+      </ol>`;
+      var ingredientsList = document.getElementById("ingredientsList")
+      for (var ingredient of ingredients) {
+        var listItem = document.createElement("li")
+        listItem.textContent = ingredient
+        console.log(listItem)
+        ingredientsList.append(listItem)
+      }
+
+      var instructionList = document.getElementById("instructionList")
+      for (var instruction of instructions) {
+        var listItem = document.createElement("li")
+        listItem.textContent = instruction
+        console.log(listItem)
+        instructionList.append(listItem)
+      }
     });
 }
 
