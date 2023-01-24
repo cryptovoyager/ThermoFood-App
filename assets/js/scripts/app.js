5b6b22a1c83609aded997cafcb3b255f55ae6ada
Raul <raulmayas20@gmail.com>
2023-01-24 15:39:28 -0600

    Filtering valuable data

--- assets/js/scripts/app.js
+++ assets/js/scripts/app.js
@@ -4,11 +4,10 @@ var ingFirstList = document.querySelectorAll("#quickIngredients");
 var insFirstList = document.querySelectorAll("#quickInstructions");
 console.log(ingFirstList);
 
-var cityAndTemperatureEL = document.getElementById('cityAndTemperature');
-var mainImageEL = document.getElementById("mainImage") //change image by api result
-var ingredientsEL = document.getElementById("ingredients") //change ingredients by API result
-var instructionEL = document.getElementById("instructions") //change instructions by APi result
-
+var cityAndTemperatureEL = document.getElementById("cityAndTemperature");
+var mainImageEL = document.getElementById("mainImage"); //change image by api result
+var ingredientsEL = document.getElementById("ingredients"); //change ingredients by API result
+var instructionEL = document.getElementById("instructions"); //change instructions by APi result
 
 function getLocation() {
   if (navigator.geolocation) {
@@ -24,7 +23,6 @@ function denyLocation() {
 
 //TODO improvement proposal: ask manual weather or location if user denies location on device
 
-
 function getApi(position) {
   var latitude = position.coords.latitude;
   var longitude = position.coords.longitude;
@@ -36,18 +34,20 @@ function getApi(position) {
       return response.json();
     })
     .then(function (data) {
+      console.log(data)
       console.log(data.city.name);
       console.log(data.list[0].main.temp);
       var temperature = parseFloat(data.list[0].main.temp);
       var city = data.city.name;
-      cityAndTemperatureEL.innerHTML = city + "  " + temperature;
+      var country = data.city.country;
+      cityAndTemperatureEL.innerHTML = city + ", "+ country + " | " + temperature + " C° ☀️";
       getFood(temperature, city);
     });
 }
 
 function getFood(temperature, city) {
   var ingredients = [];
-  var instructions = []
+  var instructions = [];
   if (temperature < 10) {
     var dishes = coldDays;
   } else if (temperature >= 10 && temperature < 25) {
@@ -80,16 +80,22 @@ function getFood(temperature, city) {
     })
     .then(function (data) {
       var index = Math.floor(Math.random() * data.results.length);
-      if (data.results[index].description == "" || data.results[index].description === null) {
-        console.log(data.results[index])
-        while (data.results[index].description == "") {
-          console.log(data.results[index])
-          index = Math.floor(Math.random() * data.results.length);
-        }
+      console.log(index)
+
+      console.log(data.results[index]);
+      console.log(data.results[index].description)
+      while (
+        !data.results[index].description ||
+        data.results[index].nutrition === undefined ||
+        data.results[index].nutrition.calories === undefined
+      ) {
+        console.log(data.results[index]);
+        index = Math.floor(Math.random() * data.results.length);
       }
 
       console.log(data.results[index]);
-      console.log(data.results[index].sections)
+      console.log(data.results[index].sections);
+      console.log(data.results[index].nutrition.calories);
       console.log("components", data.results[index].sections[0].components);
       for (var i = 0; i < data.results[index].sections.length; i++) {
         console.log("section " + data.results[index].sections[i].name);
@@ -103,16 +109,15 @@ function getFood(temperature, city) {
           );
         }
       }
-      
 
-      
       for (var i = 0; i < data.results[index].instructions.length; i++) {
         console.log(
-          "Instructions " + (i + 1) + ": " + data.results[index].instructions[i].display_text
+          "Instructions " +
+            (i + 1) +
+            ": " +
+            data.results[index].instructions[i].display_text
         );
-        instructions.push(
-          data.results[index].instructions[i].display_text
-        );
+        instructions.push(data.results[index].instructions[i].display_text);
       }
       console.log("Ingredients List:", ingredients);
       console.log("Instructions:", instructions);
@@ -128,7 +133,7 @@ function getFood(temperature, city) {
       // <ol id="instructionList">
       // </ol>`;
       //mainImage.setAttribute("src", data.results[index].thumbnail_url)
-      mainImageEL.setAttribute("src", data.results[index].thumbnail_url)
+      mainImageEL.setAttribute("src", data.results[index].thumbnail_url);
       //var ingredientsList = document.getElementById("ingredientsList")
       var dishName = document.getElementById("dishName");
       var cals = document.getElementById("calories");
@@ -136,12 +141,18 @@ function getFood(temperature, city) {
       var fats = document.getElementById("fat");
       var carbs = document.getElementById("carbs");
       var description = document.getElementById("description");
-      dishName.innerHTML = "<b>Name: </b>" + data.results[index].name
-      cals.innerHTML = "<b>Calories: </b>" + data.results[index].nutrition.calories + "kcals"
-      proteins.innerHTML = "<b>Proteins: </b>" + data.results[index].nutrition.protein + " g"
-      fats.innerHTML = "<b>Fats: </b>" + data.results[index].nutrition.fat + " g"
-      carbs.innerHTML = "<b>Carbohydrates: </b>" + data.results[index].nutrition.carbohydrates + " g"
-      description.innerHTML = data.results[index].description
+      dishName.innerHTML = "<b>Name: </b>" + data.results[index].name;
+      cals.innerHTML =
+        "<b>Calories: </b>" + data.results[index].nutrition.calories + "kcals";
+      proteins.innerHTML =
+        "<b>Proteins: </b>" + data.results[index].nutrition.protein + " g";
+      fats.innerHTML =
+        "<b>Fats: </b>" + data.results[index].nutrition.fat + " g";
+      carbs.innerHTML =
+        "<b>Carbohydrates: </b>" +
+        data.results[index].nutrition.carbohydrates +
+        " g";
+      description.innerHTML = data.results[index].description;
       console.log(data.results[index].name);
       // for (var i = 0; i < 3; i++) {
       //   ingFirstList[i].textContent = ingredients[i];
@@ -150,7 +161,7 @@ function getFood(temperature, city) {
       // for (var i = 0; i < 3; i++) {
       //   insFirstList[i].textContent = instructions[i];
       // }
-      
+
       // for (var ingredient of ingredients) {
       //   var listItem = document.createElement("li")
       //   listItem.textContent = ingredient
@@ -167,6 +178,6 @@ function getFood(temperature, city) {
       // }
     });
 }
-getLocation()
+getLocation();
 dislikeBtn.addEventListener("click", getLocation);
 // TODO safe to local storage past recipies
