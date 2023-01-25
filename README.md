87236fcb9ebaf8749568c8aaf3c5311e5252238f
Raul <raulmayas20@gmail.com>
2023-01-25 17:28:05 -0600

    Managing local storage and adding README.md

--- README.md
+++ README.md
@@ -1,3 +1,67 @@
+## Table of contents
+
+* [Introduction](#thermofood-app)
+* [User Story](#user-story)
+* [Requirements](#requirements)
+* [Getting Started](#getting-started)
+* [Apis](#apis)
+* [Future Features](#future-features)
+* [Deployed links]
+* [Credits](#credits)
+
 # ThermoFood-App
 This project focuses on helping people control their weight by selecting healthy recipes that are appropriate for the current weather. Using two APIs, one for recipes and the other for obtaining the current temperature using geolocation, the application provides options for light and fresh meals on hot days and options for more caloric meals on cold days.
 
+# Requirements
+* 🔑 Valid API key for OpenWeatherApp (developer)
+* 🔑 Valid API key for Tasty (developer)
+* 🌐 Web browser that supports HTML, CSS, and JavaScript 
+* 📶 Internet connection
+
+# User Story
+As a user, I want to find recipes based on my location and temperature to simplify meal planning.
+
+# Getting Started
+
+As a developer, you can use this app by following these steps:
+
+* Clone or download the repository to your local machine.
+* Open the index.html file in a web browser.
+* Make sure that you have an API key for the OpenWeatherMap and Tasty API. If you don't have one, you can sign up for a free account on their website.
+* Replace the placeholder text in the apiKey.js file with your own API key.
+* Make sure you have the dependencies installed, such as Tailwind CSS and FontAwesome.
+* In the app.js file, you can customize the dishes list based on temperature, and the tags you want to use to search the recipes from the Tasty API.
+* Run the application by opening the index.html file in your web browser.
+* Allow the browser to access your location and you will see a recipe based on your location and temperature.
+* You can test the app by changing your location or temperature values on the browser's developer tools, to check how it handles different temperatures and locations.
+* You can also modify the layout and styles of the app by editing the CSS file and HTML accordingly.
+
+* Note that if you're testing on a local server, the browser may block the Geolocation API, to solve this you can run the app on a web server or disable the security for the browser when testing.
+
+# APIs
+
+* The code calls the getLocation() function, which uses the browser's Geolocation API to get the user's current location (latitude and longitude).
+* The getLocation() function then makes a request to the OpenWeatherMap API using the user's latitude and longitude to get the current temperature at their location.
+* The temperature is then passed to the getFood() function, which uses the temperature to determine the type of dish to be recommended (e.g. warm, cool, hot)
+* getFood() function makes a request to the Tasty API using the temperature and get a list of recipes based on the temperature.
+* A random recipe is selected from the list of recipes and its ingredients and instructions are displayed on the webpage.
+* The e code uses the data from the Tasty API to update the webpage with the recipe information.
+
+# Future Features
+
+* The ability for the user to manually input their location instead of using the Geolocation API. This could be useful in cases where the user does not want to share their location or if the Geolocation API is not available.
+
+* The ability to filter recipes based on dietary restrictions or allergies. This could be done by adding checkboxes or dropdown menus on the webpage that allow the user to select their dietary restrictions, and then passing that information to the Tasty API when making the request for recipes.
+
+
+# Deployed Links
+
+GITHUB 
+
+
+# Credits
+* OpenWeatherMap API: The code uses this API to get the current temperature at the user's location. (https://openweathermap.org/) 
+* Tasty API: The code uses this API to get the list of recipes based on the temperature. (https://rapidapi.com/tasty/api/tasty) 
+* Geolocation API (https://developer.mozilla.org/en-US/docs/Web/API/Geolocation_API)
+* TailwindCSS: The structure and stylings are made using Tailwind. 
+* Tailwind Components: The structure use a layout from (https://tailwindui.com/components)
--- assets/js/scripts/app.js
+++ assets/js/scripts/app.js
@@ -6,8 +6,138 @@ var ingrCloseModal = document.querySelector("#ingrCloseModal");
 var insCloseModal = document.querySelector("#insCloseModal");
 var ingredientsList = document.getElementById("ingredientsList");
 var instructionsList = document.getElementById("instructionsList");
+var likeBtn = document.getElementById("like");
+var urRecipies = document.getElementById("urRecipies")
 
 var mainImageEL = document.getElementById("mainImage"); //change image by api result
+
+function gettingDishes(dishes) {
+  var ingredients = [];
+  var instructions = [];
+  {
+    var dishesIndex = Math.floor(Math.random() * dishes.length);
+    console.log(dishes);
+    console.log(dishesIndex);
+    var tag = dishes[dishesIndex];
+    console.log(tag);
+
+    const options = {
+      method: "GET",
+      headers: {
+        "X-RapidAPI-Key": "a1190a5613mshb896d0d8d85cd92p117dddjsn28b788dac426",
+        "X-RapidAPI-Host": "tasty.p.rapidapi.com",
+      },
+    };
+
+    fetch(
+      `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=${tag}`,
+      //"https://tasty.p.rapidapi.com/tags/list",
+      options
+    )
+      .then(function (response) {
+        return response.json();
+      })
+      .then(function (data) {
+        var index = Math.floor(Math.random() * data.results.length);
+        console.log(index);
+        console.log(data.results);
+        console.log(data.results[index]);
+        if (data.results.length === 0) {
+          getLocation();
+        }
+        console.log(data.results[index].description);
+        while (
+          data.results[index].nutrition === undefined ||
+          data.results[index].nutrition.calories === undefined
+        ) {
+          console.log(data.results[index]);
+          console.log(index);
+          index = Math.floor(Math.random() * data.results.length);
+          console.log(index);
+        }
+
+        console.log(data.results[index]);
+        console.log(data.results[index].sections);
+        console.log(data.results[index].nutrition.calories);
+        console.log("components", data.results[index].sections[0].components);
+        for (var i = 0; i < data.results[index].sections.length; i++) {
+          console.log("section " + data.results[index].sections[i].name);
+          for (
+            var j = 0;
+            j < data.results[index].sections[i].components.length;
+            j++
+          ) {
+            ingredients.push(
+              data.results[index].sections[i].components[j].raw_text
+            );
+          }
+        }
+
+        for (var i = 0; i < data.results[index].instructions.length; i++) {
+          console.log(
+            "Instructions " +
+              (i + 1) +
+              ": " +
+              data.results[index].instructions[i].display_text
+          );
+          instructions.push(data.results[index].instructions[i].display_text);
+        }
+        console.log("Ingredients List:", ingredients);
+        console.log("Instructions:", instructions);
+        mainImageEL.setAttribute("src", data.results[index].thumbnail_url);
+        var dishName = document.getElementById("dishName");
+        var cals = document.getElementById("calories");
+        var proteins = document.getElementById("proteins");
+        var fats = document.getElementById("fat");
+        var carbs = document.getElementById("carbs");
+        var description = document.getElementById("description");
+        dishName.innerHTML = "<b>Name: </b>" + data.results[index].name;
+        cals.innerHTML =
+          "<b>Calories: </b>" +
+          data.results[index].nutrition.calories +
+          "kcals";
+        proteins.innerHTML =
+          "<b>Proteins: </b>" + data.results[index].nutrition.protein + " g";
+        fats.innerHTML =
+          "<b>Fats: </b>" + data.results[index].nutrition.fat + " g";
+        carbs.innerHTML =
+          "<b>Carbohydrates: </b>" +
+          data.results[index].nutrition.carbohydrates +
+          " g";
+        description.innerHTML = data.results[index].description;
+        console.log(data.results[index].name);
+        ingredientsList.innerHTML = "";
+        for (var ingredient of ingredients) {
+          var listItem = document.createElement("li");
+          listItem.classList.add("flex");
+          listItem.classList.add("items-start");
+          listItem.innerHTML = `<div class="flex-shrink-0"> <svg class="h-6 w-6 flex-shrink-0 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none"           viewBox="0 0 24 24"           stroke-width="1.5"           stroke="currentColor"           aria-hidden="true"         >           <path             stroke-linecap="round"             stroke-linejoin="round"             d="M4.5 12.75l6 6 9-13.5"           />         </svg>       </div>       <p class="ml-3 text-base font-medium text-gray-500">         ${ingredient}       </p>`;
+          ingredientsList.append(listItem);
+        }
+
+        instructionsList.innerHTML = "";
+        for (var instruction of instructions) {
+          var listItem = document.createElement("li");
+          listItem.classList.add("flex");
+          listItem.classList.add("items-start");
+          listItem.innerHTML = `<div class="flex-shrink-0"> <svg class="h-6 w-6 flex-shrink-0 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none"           viewBox="0 0 24 24"           stroke-width="1.5"           stroke="currentColor"           aria-hidden="true"         >           <path             stroke-linecap="round"             stroke-linejoin="round"             d="M4.5 12.75l6 6 9-13.5"           />         </svg>       </div>       <p class="ml-3 text-base font-medium text-gray-500">         ${instruction}       </p>`;
+          instructionsList.append(listItem);
+        }
+
+        likeBtn.addEventListener("click", function () {
+          console.log("image",  data.results[index].thumbnail_url)
+          console.log("name: ",data.results[index].name);
+          console.log("calories: ", data.results[index].nutrition.calories);
+          console.log("carbs: ", data.results[index].nutrition.carbohydrates);
+          console.log("proteins: ", data.results[index].nutrition.protein);
+          console.log("fats: ", data.results[index].nutrition.fat);
+          console.log("description: ", data.results[index].description);
+          console.log("ings: ",ingredients);
+          console.log("ins: ",instructions);
+        });
+      });
+  }
+}
 
 function showIngredientsHandler() {
   var ingModal = document.getElementById("ingredientsModal");
@@ -28,8 +158,6 @@ function getLocation() {
 }
 
 function denyLocation() {
-  var ingredients = [];
-  var instructions = [];
   var today = dayjs();
   var hour = today.format("HH");
   statusEl.innerHTML = today.format("DD-MMM-YYYY  hh:mm:ss A");
@@ -40,113 +168,7 @@ function denyLocation() {
   } else {
     var dishes = dinner;
   }
-  var dishesIndex = Math.floor(Math.random() * dishes.length);
-  console.log(dishes);
-  console.log(dishesIndex);
-  var tag = dishes[dishesIndex];
-  console.log(tag);
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a1190a5613mshb896d0d8d85cd92p117dddjsn28b788dac426",
-      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-    },
-  };
-
-  fetch(
-    `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=${tag}`,
-    //"https://tasty.p.rapidapi.com/tags/list",
-    options
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var index = Math.floor(Math.random() * data.results.length);
-      console.log(index);
-      console.log(data.results);
-      console.log(data.results[index]);
-      if (data.results.length === 0) {
-        getLocation();
-      }
-      console.log(data.results[index].description);
-      while (
-        data.results[index].nutrition === undefined ||
-        data.results[index].nutrition.calories === undefined
-      ) {
-        console.log(data.results[index]);
-        console.log(index);
-        index = Math.floor(Math.random() * data.results.length);
-        console.log(index);
-      }
-
-      console.log(data.results[index]);
-      console.log(data.results[index].sections);
-      console.log(data.results[index].nutrition.calories);
-      console.log("components", data.results[index].sections[0].components);
-      for (var i = 0; i < data.results[index].sections.length; i++) {
-        console.log("section " + data.results[index].sections[i].name);
-        for (
-          var j = 0;
-          j < data.results[index].sections[i].components.length;
-          j++
-        ) {
-          ingredients.push(
-            data.results[index].sections[i].components[j].raw_text
-          );
-        }
-      }
-
-      for (var i = 0; i < data.results[index].instructions.length; i++) {
-        console.log(
-          "Instructions " +
-            (i + 1) +
-            ": " +
-            data.results[index].instructions[i].display_text
-        );
-        instructions.push(data.results[index].instructions[i].display_text);
-      }
-      console.log("Ingredients List:", ingredients);
-      console.log("Instructions:", instructions);
-      mainImageEL.setAttribute("src", data.results[index].thumbnail_url);
-      var dishName = document.getElementById("dishName");
-      var cals = document.getElementById("calories");
-      var proteins = document.getElementById("proteins");
-      var fats = document.getElementById("fat");
-      var carbs = document.getElementById("carbs");
-      var description = document.getElementById("description");
-      dishName.innerHTML = "<b>Name: </b>" + data.results[index].name;
-      cals.innerHTML =
-        "<b>Calories: </b>" + data.results[index].nutrition.calories + "kcals";
-      proteins.innerHTML =
-        "<b>Proteins: </b>" + data.results[index].nutrition.protein + " g";
-      fats.innerHTML =
-        "<b>Fats: </b>" + data.results[index].nutrition.fat + " g";
-      carbs.innerHTML =
-        "<b>Carbohydrates: </b>" +
-        data.results[index].nutrition.carbohydrates +
-        " g";
-      description.innerHTML = data.results[index].description;
-      console.log(data.results[index].name);
-      ingredientsList.innerHTML = "";
-      for (var ingredient of ingredients) {
-        var listItem = document.createElement("li");
-        listItem.classList.add("flex");
-        listItem.classList.add("items-start");
-        listItem.innerHTML = `<div class="flex-shrink-0"> <svg class="h-6 w-6 flex-shrink-0 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none"           viewBox="0 0 24 24"           stroke-width="1.5"           stroke="currentColor"           aria-hidden="true"         >           <path             stroke-linecap="round"             stroke-linejoin="round"             d="M4.5 12.75l6 6 9-13.5"           />         </svg>       </div>       <p class="ml-3 text-base font-medium text-gray-500">         ${ingredient}       </p>`;
-        ingredientsList.append(listItem);
-      }
-
-      instructionsList.innerHTML = "";
-      for (var instruction of instructions) {
-        var listItem = document.createElement("li");
-        listItem.classList.add("flex");
-        listItem.classList.add("items-start");
-        listItem.innerHTML = `<div class="flex-shrink-0"> <svg class="h-6 w-6 flex-shrink-0 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none"           viewBox="0 0 24 24"           stroke-width="1.5"           stroke="currentColor"           aria-hidden="true"         >           <path             stroke-linecap="round"             stroke-linejoin="round"             d="M4.5 12.75l6 6 9-13.5"           />         </svg>       </div>       <p class="ml-3 text-base font-medium text-gray-500">         ${instruction}       </p>`;
-        instructionsList.append(listItem);
-      }
-    });
+  gettingDishes(dishes);
 }
 
 //TODO improvement proposal: ask manual weather or location if user denies location on device
@@ -180,8 +202,6 @@ function getApi(position) {
 }
 
 function getFood(temperature, city) {
-  var ingredients = [];
-  var instructions = [];
   if (temperature < 10) {
     var dishes = coldDays;
   } else if (temperature >= 10 && temperature < 25) {
@@ -191,112 +211,7 @@ function getFood(temperature, city) {
   } else {
     var dishes = hotDays;
   }
-  var dishesIndex = Math.floor(Math.random() * dishes.length);
-  console.log(dishesIndex);
-  var tag = dishes[dishesIndex];
-  console.log(tag);
-
-  const options = {
-    method: "GET",
-    headers: {
-      "X-RapidAPI-Key": "a1190a5613mshb896d0d8d85cd92p117dddjsn28b788dac426",
-      "X-RapidAPI-Host": "tasty.p.rapidapi.com",
-    },
-  };
-
-  fetch(
-    `https://tasty.p.rapidapi.com/recipes/list?from=0&size=20&tags=${tag}`,
-    //"https://tasty.p.rapidapi.com/tags/list",
-    options
-  )
-    .then(function (response) {
-      return response.json();
-    })
-    .then(function (data) {
-      var index = Math.floor(Math.random() * data.results.length);
-      console.log(index);
-      console.log(data.results);
-      console.log(data.results[index]);
-      if (data.results.length === 0) {
-        getLocation();
-      }
-      console.log(data.results[index].description);
-      while (
-        data.results[index].nutrition === undefined ||
-        data.results[index].nutrition.calories === undefined
-      ) {
-        console.log(data.results[index]);
-        console.log(index);
-        index = Math.floor(Math.random() * data.results.length);
-        console.log(index);
-      }
-
-      console.log(data.results[index]);
-      console.log(data.results[index].sections);
-      console.log(data.results[index].nutrition.calories);
-      console.log("components", data.results[index].sections[0].components);
-      for (var i = 0; i < data.results[index].sections.length; i++) {
-        console.log("section " + data.results[index].sections[i].name);
-        for (
-          var j = 0;
-          j < data.results[index].sections[i].components.length;
-          j++
-        ) {
-          ingredients.push(
-            data.results[index].sections[i].components[j].raw_text
-          );
-        }
-      }
-
-      for (var i = 0; i < data.results[index].instructions.length; i++) {
-        console.log(
-          "Instructions " +
-            (i + 1) +
-            ": " +
-            data.results[index].instructions[i].display_text
-        );
-        instructions.push(data.results[index].instructions[i].display_text);
-      }
-      console.log("Ingredients List:", ingredients);
-      console.log("Instructions:", instructions);
-      mainImageEL.setAttribute("src", data.results[index].thumbnail_url);
-      var dishName = document.getElementById("dishName");
-      var cals = document.getElementById("calories");
-      var proteins = document.getElementById("proteins");
-      var fats = document.getElementById("fat");
-      var carbs = document.getElementById("carbs");
-      var description = document.getElementById("description");
-      dishName.innerHTML = "<b>Name: </b>" + data.results[index].name;
-      cals.innerHTML =
-        "<b>Calories: </b>" + data.results[index].nutrition.calories + "kcals";
-      proteins.innerHTML =
-        "<b>Proteins: </b>" + data.results[index].nutrition.protein + " g";
-      fats.innerHTML =
-        "<b>Fats: </b>" + data.results[index].nutrition.fat + " g";
-      carbs.innerHTML =
-        "<b>Carbohydrates: </b>" +
-        data.results[index].nutrition.carbohydrates +
-        " g";
-      description.innerHTML = data.results[index].description;
-      console.log(data.results[index].name);
-      ingredientsList.innerHTML = "";
-      for (var ingredient of ingredients) {
-        var listItem = document.createElement("li");
-        listItem.classList.add("flex");
-        listItem.classList.add("items-start");
-        listItem.innerHTML = `<div class="flex-shrink-0"> <svg class="h-6 w-6 flex-shrink-0 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none"           viewBox="0 0 24 24"           stroke-width="1.5"           stroke="currentColor"           aria-hidden="true"         >           <path             stroke-linecap="round"             stroke-linejoin="round"             d="M4.5 12.75l6 6 9-13.5"           />         </svg>       </div>       <p class="ml-3 text-base font-medium text-gray-500">         ${ingredient}       </p>`;
-        ingredientsList.append(listItem);
-      }
-
-      instructionsList.innerHTML = "";
-      for (var instruction of instructions) {
-        var listItem = document.createElement("li");
-        listItem.classList.add("flex");
-        listItem.classList.add("items-start");
-        listItem.innerHTML = `<div class="flex-shrink-0"> <svg class="h-6 w-6 flex-shrink-0 text-green-500" xmlns="http://www.w3.org/2000/svg" fill="none"           viewBox="0 0 24 24"           stroke-width="1.5"           stroke="currentColor"           aria-hidden="true"         >           <path             stroke-linecap="round"             stroke-linejoin="round"             d="M4.5 12.75l6 6 9-13.5"           />         </svg>       </div>       <p class="ml-3 text-base font-medium text-gray-500">         ${instruction}       </p>`;
-        instructionsList.append(listItem);
-      }
-    });
+  gettingDishes(dishes);
 }
 getLocation();
 dislikeBtn.addEventListener("click", getLocation);
